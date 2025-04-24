973a76081e431895a58d13d8ef75ec990b6b08dd
jay <135799237+jay-bman725@users.noreply.github.com>
2025-04-24 12:31:08 -0500

    Revert "hope this works (again)"
    This reverts commit 5348c760394c58c76be276d11f581e81eadc585d.

--- src/index.js
+++ src/index.js
@@ -3,22 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
+import { PostHogProvider} from 'posthog-js/react'
 
-// Initialize PostHog with a placeholder key
-// Note: Replace this with your actual key in production
-posthog.init('phc_placeholder', {
-  api_host: 'https://app.posthog.com',
-  loaded: (posthog) => {
-    if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing();
-  }
-});
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PostHogProvider client={posthog}>
+    <PostHogProvider 
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
       <App />
     </PostHogProvider>
   </React.StrictMode>
